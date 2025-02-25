e184123143d1d0c77193042f47e8c4fc36289a6c
Шацков Дмитрий <dima.shaczkov@mail.ru>
2025-02-25 10:38:20 +0300

    -tasks completed

--- Assets/Scripts/EditorMover.cs
+++ Assets/Scripts/EditorMover.cs
@@ -8,14 +8,22 @@ namespace DefaultNamespace
 	{
 		private PositionSaver _save;
 		private float _currentDelay;
-		
+
 		//todo comment: Что произойдёт, если _delay > _duration?
+		//Не сохранится ни одной позиции.
+		[SerializeField, Range(0.2f, 1.0f)]
 		private float _delay = 0.5f;
+		[SerializeField, Min(0.2f)]
 		private float _duration = 5f;
 
 		private void Start()
 		{
+			if(_duration < _delay)
+			{
+				_duration = 5 * _delay;
+			}
 			//todo comment: Почему этот поиск производится здесь, а не в начале метода Update?
+			//Если вызывать его в Update, то при каждом обновлении кадра список будет очищаться
 			_save = GetComponent<PositionSaver>();
 			_save.Records.Clear();
 		}
@@ -31,6 +39,7 @@ namespace DefaultNamespace
 			}
 			
 			//todo comment: Почему не написать (_delay -= Time.deltaTime;) по аналогии с полем _duration?
+			//deltaTime может отличаться в зависимости от fps, а нам необходимо записывать свою позицию каждый _delay секунд. В случае с _duration нам не требуется такой точности.
 			_currentDelay -= Time.deltaTime;
 			if (_currentDelay <= 0f)
 			{
@@ -39,6 +48,7 @@ namespace DefaultNamespace
 				{
 					Position = transform.position,
 					//todo comment: Для чего сохраняется значение игрового времени?
+					//Для дальнейшего использования в ReplayMover
 					Time = Time.time,
 				});
 			}
--- Assets/Scripts/PositionSaver.cs
+++ Assets/Scripts/PositionSaver.cs
@@ -7,19 +7,23 @@ namespace DefaultNamespace
 {
 	public class PositionSaver : MonoBehaviour
 	{
+		[Serializable]
 		public struct Data
 		{
-			public Vector3 Position;
+            public Vector3 Position;
 			public float Time;
 		}
 
-		private TextAsset _json;
+		[ReadOnly, Tooltip("Для заполнения этого поля нужно воспользоваться контекстным меню в инспекторе и командой 'Create File'")]
+		public TextAsset _json;
 
-		public List<Data> Records { get; private set; }
+		[SerializeField, HideInInspector]
+		public List<Data> Records = new List<Data>();
 
 		private void Awake()
 		{
 			//todo comment: Что будет, если в теле этого условия не сделать выход из метода?
+			//Программа продолжит свое выполнение и будем получать ошибку в методе FromJsonOverwrite.
 			if (_json == null)
 			{
 				gameObject.SetActive(false);
@@ -29,6 +33,8 @@ namespace DefaultNamespace
 			
 			JsonUtility.FromJsonOverwrite(_json.text, this);
 			//todo comment: Для чего нужна эта проверка (что она позволяет избежать)?
+			//Ошибка NullReference
+
 			if (Records == null)
 				Records = new List<Data>(10);
 		}
@@ -36,12 +42,15 @@ namespace DefaultNamespace
 		private void OnDrawGizmos()
 		{
 			//todo comment: Зачем нужны эти проверки (что они позволляют избежать)?
+			//Мы не работаем с листом, если он пуст. И аналогично выше с проверкой на Null.
+
 			if (Records == null || Records.Count == 0) return;
 			var data = Records;
 			var prev = data[0].Position;
 			Gizmos.color = Color.green;
 			Gizmos.DrawWireSphere(prev, 0.3f);
 			//todo comment: Почему итерация начинается не с нулевого элемента?
+			//Так как нулевой элемент уже записан в переменную prev.
 			for (int i = 1; i < data.Count; i++)
 			{
 				var curr = data[i].Position;
@@ -56,8 +65,10 @@ namespace DefaultNamespace
 		private void CreateFile()
 		{
 			//todo comment: Что происходит в этой строке?
+			//Будет создан файл Path.txt с комбинированным путем в application
 			var stream = File.Create(Path.Combine(Application.dataPath, "Path.txt"));
 			//todo comment: Подумайте для чего нужна эта строка? (а потом проверьте догадку, закомментировав) 
+			//Для дальнейшей работы с файлом, если не закрыть поток, то при записи в файл будем получать ошибку
 			stream.Dispose();
 			UnityEditor.AssetDatabase.Refresh();
 			//В Unity можно искать объекты по их типу, для этого используется префикс "t:"
@@ -70,6 +81,7 @@ namespace DefaultNamespace
 				//Этой командой можно загрузить сам ассет
 				var asset = UnityEditor.AssetDatabase.LoadAssetAtPath<TextAsset>(path);
 				//todo comment: Для чего нужны эти проверки?
+				//Проверяем был ли загружен asset, проверяем чтобы загрузился именно наш asset, который был создан выше
 				if(asset != null && asset.name == "Path")
 				{
 					_json = asset;
@@ -77,6 +89,7 @@ namespace DefaultNamespace
 					UnityEditor.AssetDatabase.SaveAssets();
 					UnityEditor.AssetDatabase.Refresh();
 					//todo comment: Почему мы здесь выходим, а не продолжаем итерироваться?
+					//Возможно из-за того, что существует лишь один asset с именем Path
 					return;
 				}
 			}
@@ -84,8 +97,19 @@ namespace DefaultNamespace
 
 		private void OnDestroy()
 		{
-			//todo logic...
-		}
+			if (_json == null) 
+			{ 
+				return; 
+			}
+			var text = JsonUtility.ToJson(this, true);
+			var path = UnityEditor.AssetDatabase.GetAssetPath(_json);
+
+			path = Path.Combine(Application.dataPath.Replace("Assets",""), path);
+            File.WriteAllText(path, text);
+			UnityEditor.EditorUtility.SetDirty(_json);
+			UnityEditor.AssetDatabase.SaveAssets();
+			UnityEditor.AssetDatabase.Refresh();
+        }
 #endif
 	}
 }
